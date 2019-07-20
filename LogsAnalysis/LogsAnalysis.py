b6e6a6500a8a89bcf3c1297a3cb1611c8555d594
AboSalaah <abosalaah96@gmail.com>
2019-07-20 18:31:51 +0200

    add most popular three articles function

---
+++ LogsAnalysis/LogsAnalysis.py
@@ -0,0 +1,24 @@
+import psycopg2
+import datetime
+DBNAME = "news"
+
+
+# querying the database for the most popular three articles of all time
+def mostPopularArticles():
+    print("\nThe most popular three articles of all time:\n")
+    db = psycopg2.connect(database=DBNAME)
+    cursor = db.cursor()
+    cursor.execute(
+        "SELECT articles.title, count(log.id) AS Views "
+        "FROM log, articles "
+        "WHERE log.path LIKE '%article%' "
+        "AND (substring(log.path FROM 10) = articles.slug) "
+        "GROUP BY articles.title "
+        "ORDER BY Views DESC LIMIT 3")
+    results = cursor.fetchall()
+    for row in results:
+        print(row[0] + " --- " + str(row[1]) + " Views")
+
+    cursor.close()
+    db.close()
+
